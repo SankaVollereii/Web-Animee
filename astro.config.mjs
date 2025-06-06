310096e82f5b0bd279048907513bfd7fc912c5b8
Sanka Vollereii <arigowok05@gmail.com>
2025-06-07 00:13:47 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
+import vercel from '@astrojs/vercel/serverless'; // atau /static jika statis
 
 import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
+output: 'server', // Penting untuk SSR
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
     build: {
