aced46175fa1bf4c82427997606b8fc4baad5763
Sanka Vollereii <arigowok05@gmail.com>
2025-06-07 00:16:43 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
-import vercel from '@astrojs/vercel/serverless'; // atau /static jika statis
+import vercel from '@astrojs/vercel'; // atau /static jika statis
 
 import playformCompress from "@playform/compress";
 
